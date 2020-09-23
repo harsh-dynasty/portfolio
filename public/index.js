859f742990a82d5aa313fcd5aba52ea09938bfd1
harsh-dynasty <66057922+harsh-dynasty@users.noreply.github.com>
2020-09-23 18:24:25 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -59,46 +59,8 @@ function submitForm(){
     
 }
 //google pay api integration
-const supportedInstruments = [
-  {
-    supportedMethods: ['https://tez.google.com/pay'],
-    data: {
-      pa: 'harshsoniji@ybl',
-      pn: 'Harsh Soni',
-      tr: '1234ABCD',  // your custom transaction reference ID
-      url: 'http://url/of/the/order/in/your/website',
-      mc: '1234', // your merchant category code
-      tn: 'Purchase in Merchant',
-    },
-  }
-];
-const details = {
-  total: {
-    label: 'Total',
-    amount: {
-      currency: 'INR',
-      value: '10', // sample amount
-    },
-  },
-  displayItems: [{
-    label: 'Original Amount',
-    amount: {
-      currency: 'INR',
-      value: '10',
-    },
-  }],
-};
-let request = null;
-try {
-  request = new PaymentRequest(supportedInstruments, details);
-} catch (e) {
-  console.log('Payment Request Error: ' + e.message);
-  // return;
-}
-if (!request) {
-  console.log('Web payments are not supported in this browser.');
-  // return;
-}
+
+
 const canMakePaymentCache = 'canMakePaymentCache';
 
 function checkCanMakePayment(request) {
